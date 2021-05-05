7bb0da08067645c3047ba55f62e894667e5e3fb9
Anna-Maria Haslinger <anna-maria.haslinger@gmx.de>
2021-05-05 21:10:28 +0200

    TS ausgelagert

---
+++ src/domUtils.ts
@@ -0,0 +1,29 @@
+export let  backschritte = document.getElementById('backschritte') as HTMLDivElement;
+export let  backschritteRZ = document.getElementById('backschritteRZ') as HTMLDivElement;
+export let  backschritteEK = document.getElementById('backschritteEK') as HTMLDivElement;
+
+export const brownieButton=document.getElementById('bButton') as HTMLElement;
+export const zupfkuchenButton = document.getElementById('rzButton') as HTMLElement;
+export const erdbeerkuchenButton = document.getElementById('ekButton') as HTMLElement;
+
+export const schritt1 = document.getElementById('b1') as HTMLElement;
+export const schritt2 = document.getElementById('b2') as HTMLElement;
+export const schritt3 = document.getElementById('b3') as HTMLElement;
+export const schritt4 = document.getElementById('b4') as HTMLElement;
+export const schritt5 = document.getElementById('b5') as HTMLElement;
+
+export const schritt1RZ = document.getElementById('rz1') as HTMLElement;
+export const schritt2RZ = document.getElementById('rz2') as HTMLElement;
+export const schritt3RZ = document.getElementById('rz3') as HTMLElement;
+export const schritt4RZ = document.getElementById('rz4') as HTMLElement;
+
+export const schritt1EK = document.getElementById('ek1') as HTMLElement;
+export const schritt2EK = document.getElementById('ek2') as HTMLElement;
+export const schritt3EK = document.getElementById('ek3') as HTMLElement;
+export const schritt4EK = document.getElementById('ek4') as HTMLElement;
+export const schritt5EK = document.getElementById('ek5') as HTMLElement;
+
+export const closeButtonRz = document.getElementById('closeRzModal') as HTMLElement;
+export const closeButtonB = document.getElementById('closeBModal') as HTMLElement;
+export const closeButtonEk = document.getElementById('closeEkModal') as HTMLElement;
+
